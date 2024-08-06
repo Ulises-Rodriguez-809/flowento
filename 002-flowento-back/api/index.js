393d33080380b416823634731b23b4f2ae3cfa50
Ulises <uliisesrodriguez809@gmail.com>
2024-08-06 19:23:44 -0300

    añadiendo config vercel deploy

--- 002-flowento-back/api/index.js
+++ 002-flowento-back/api/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import app from '../src/app.js';
+// import app from '../src/app.js';
 
 const app = express();
 
@@ -7,4 +7,4 @@ app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-export default app;
+// export default app;
